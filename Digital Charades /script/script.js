554cf15c753d7f1e415851b166aab85d3cec014f
Dave146 <90393898+Dave-146@users.noreply.github.com>
2023-04-12 16:27:10 -0400

    update

--- Digital Charades /script/script.js
+++ Digital Charades /script/script.js
@@ -4,18 +4,18 @@ const spinBtn = document.getElementById("spin_btn");
 const text = document.getElementById("text");
 /* --------------- Minimum And Maximum Angle For A value  --------------------- */
 const spinValues = [
-  { minDegree: 61, maxDegree: 90, value: "dancing 1" },
-  { minDegree: 31, maxDegree: 60, value: "dancing 2" },
-  { minDegree: 0, maxDegree: 30, value: "dancing 3" },
-  { minDegree: 331, maxDegree: 360, value: "dancing 4" },
-  { minDegree: 301, maxDegree: 330, value: "dancing 5" },
-  { minDegree: 271, maxDegree: 300, value: "dancing 6" },
-  { minDegree: 241, maxDegree: 270, value: "dancing 7" },
-  { minDegree: 211, maxDegree: 240, value: "dancing 8" },
-  { minDegree: 181, maxDegree: 210, value: "dancing 9" },
-  { minDegree: 151, maxDegree: 180, value: "dancing 10" },
-  { minDegree: 121, maxDegree: 150, value: "dancing 11" },
-  { minDegree: 91, maxDegree: 120, value: "dancing 12" },
+  { minDegree: 61, maxDegree: 90, value: "You are brushing your teeth" },
+  { minDegree: 31, maxDegree: 60, value: "You are doing a magic trick" },
+  { minDegree: 0, maxDegree: 30, value: "You are playing the piano" },
+  { minDegree: 331, maxDegree: 360, value: "You are playing basketball" },
+  { minDegree: 301, maxDegree: 330, value: "You are taking a selfie" },
+  { minDegree: 271, maxDegree: 300, value: "You are doing a yoga pose" },
+  { minDegree: 241, maxDegree: 270, value: "You are baking a cake" },
+  { minDegree: 211, maxDegree: 240, value: "You are walking a dog" },
+  { minDegree: 181, maxDegree: 210, value: "You are opening a present" },
+  { minDegree: 151, maxDegree: 180, value: "You are playing video games" },
+  { minDegree: 121, maxDegree: 150, value: "You are fishing" },
+  { minDegree: 91, maxDegree: 120, value: "You are rock climbing" },
 ];
 /* --------------- Size Of Each Piece  --------------------- */
 const size = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
@@ -69,7 +69,7 @@ let spinChart = new Chart(spinWheel, {
 const generateValue = (angleValue) => {
   for (let i of spinValues) {
     if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
-      text.innerHTML = `<p>Act as a ${i.value} ! </p>`;
+      text.innerHTML = `<p>Act as if "${i.value}" </p>`;
       spinBtn.disabled = false;
       break;
     }
@@ -80,7 +80,7 @@ let count = 0;
 let resultValue = 101;
 spinBtn.addEventListener("click", () => {
   spinBtn.disabled = true;
-  text.innerHTML = `<p>Lets get ready to Mime</p>`;
+  text.innerHTML = `<p>Get ready to Spoof!</p>`;
   let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
   let rotationInterval = window.setInterval(() => {
     spinChart.options.rotation = spinChart.options.rotation + resultValue;
